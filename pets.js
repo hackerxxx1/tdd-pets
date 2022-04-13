340df660f76e1ef116870b0cba33ac6db6e32161
hackerxxx1 <43994798+hackerxxx1@users.noreply.github.com>
2022-04-13 18:01:06 -0300

    att atividade feita

--- pets.js
+++ pets.js
@@ -1,23 +1,60 @@
-const data = [
-    {id:1, nome:"Capitu", idade:15, sexo:"M", especie:"Calopisita"},
-    {id:2, nome:"Xuxu", idade:5, sexo:"M", especie:"Gato"},
-    {id:3, nome:"Mag", idade:10, sexo:"F", especie:"Galinha"},
-    {id:4, nome:"Zezinho", idade:11, sexo:"M", especie:"Tartaruga"},
-    {id:5, nome:"Gertrudi", idade:12, sexo:"F", especie:"Mula"},
 
+const data = [
+    { id: 1, nome: "Capitu", 
+        idade: 15, sexo: "F",
+        especie: "Calopsita"
+    },
+    {
+      id: 2, nome: "Bentinho",
+        idade: 5, sexo: "M",
+        especie: "Cachorro"
+    }
 ];
 
-function listar() {return data}
-function listarPorId(id) {let pet = data.find(pet => pet.id == id)
-     if(pet){return pet}else{return({erro: "True",mensagem:"Pet não encontrado!"})}}
-function cadastrar(x) {
-    x.id=data.length + 1
-    data.push(x);
-    return x
+function listar() {
+    return data;
+}
+
+function listarPorId(id) {
+    const pet = data.find(pet => pet.id === id)
+    if(!pet) {
+        return {
+            erro: true,
+            mensagem: "Pet não encontrado!"
+        };
+    }
+    return pet;
+}
+
+function cadastrar(pet) {
+    if(!(pet.hasOwnProperty('nome'))){return ({erro: true,mensagem: "Campo 'nome' é obrigatório!"})}
+    else if(!(pet.hasOwnProperty('sexo'))){return ({erro: true,mensagem: "Campo 'sexo' é obrigatório!"})}
+    else if(!(pet.hasOwnProperty('idade'))){return ({erro: true,mensagem: "Campo 'idade' é obrigatório!"})}
+    else if(!(pet.hasOwnProperty('especie'))){return ({erro: true,mensagem: "Campo 'especie' é obrigatório!"})}
+    else if(pet.idade<0){ return {erro: true, mensagem: "Campo 'idade' deve ser maior ou igual a zero!"}}
+    else if(pet.sexo !=( 'M'|| 'F')){return{erro: true,mensagem: "Campo 'sexo' deve ser 'M' ou 'F'!"}}
+    else{pet.id = data.length+1;data.push(pet);return pet;}
+}
+
+function editarPorId(id, pet) {
+    const index = data.findIndex(pet => pet.id === id);
+    if(pet.idade<0){return {erro: true, mensagem: "Campo 'idade' deve ser maior ou igual a zero!"}}else if (pet.sexo !=( 'M'|| 'F')){return{erro: true,mensagem: "Campo 'sexo' deve ser 'M' ou 'F'!"}}
+    else
+    if(index === -1){
+        return {erro: true, mensagem: "Pet não encontrado!"}}
+        else{pet.id = index;data[index]= pet;return data[index]}
+}
+
+function deletarPorId(id) {
+    const index = data.findIndex(pet => pet.id === id);
+    if(index === -1){return {erro: true, mensagem: "Pet não encontrado!"}}
+    else{data.splice(index,1); return {erro: false, mensagem: "Pet deletado com sucesso!"}}
 }
 
 module.exports = {
-    listar:listar,
-    listarPorId:listarPorId,
-    cadastrar:cadastrar,
+    listar,
+    listarPorId,
+    cadastrar,
+    editarPorId,
+    deletarPorId
 }
